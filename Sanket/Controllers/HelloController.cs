863f720408b7e1356434b1e84733227ebd7b01e0
S-anket-kum <sanketkmr01@gmail.com>
2023-03-11 22:17:49 +0530

    Add project files.

---
+++ Sanket/Controllers/HelloController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sanket.Models;
+
+namespace Sanket.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Student()
+        {
+            Student student = new Student
+            {
+                Id = 1,
+                Name = "Sanket",
+                Gender = "Male"
+            };
+
+            return View(student);
+
+        }
+    }
+}
